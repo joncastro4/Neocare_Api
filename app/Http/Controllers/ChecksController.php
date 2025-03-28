c24b0695d0b9095decbc6fd067645d4073057085
Jonathan Castro <uaoaar777@gmail.com>
2025-03-27 18:03:28 -0600

    Update ChecksController.php

--- app/Http/Controllers/ChecksController.php
+++ app/Http/Controllers/ChecksController.php
@@ -8,6 +8,7 @@ use App\Models\Check;
 use App\Models\Nurse;
 use Illuminate\Support\Facades\Validator;
 use App\Models\UserPerson;
+use Laravel\Sanctum\PersonalAccessToken;
 
 class ChecksController extends Controller
 {
@@ -21,90 +22,101 @@ class ChecksController extends Controller
             'date1' => 'nullable|date|before_or_equal:date2|before_or_equal:today',
             'date2' => 'nullable|date|after_or_equal:date1|before_or_equal:today',
         ]);
-
+    
         if ($validate->fails()) {
             return response()->json([
                 'errors' => $validate->errors()
             ], 422);
         }
+    
+        $token = $request->bearerToken();
 
-        $user = auth()->user();
-
+        $user = PersonalAccessToken::findToken($token)->tokenable;
+    
         if ($user->role == 'nurse' || $user->role == 'nurse-admin') {
             $userPerson = UserPerson::where('user_id', $user->id)->first();
-
+    
             if (!$userPerson) {
                 return response()->json([
                     'message' => 'UserPerson not found'
                 ], 404);
             }
-
+    
             $nurse = Nurse::where('user_person_id', $userPerson->id)->first();
-
+    
             if (!$nurse) {
                 return response()->json([
                     'message' => 'Nurse not found'
                 ], 404);
             }
-
+    
             $hospital = $nurse->hospital;
-
+    
+            $checksQuery = Check::query()->with(['nurse.userPerson.person', 'baby_incubator.baby.person']);
+    
             if ($user->role == 'nurse-admin') {
                 $nurses = Nurse::where('hospital_id', $hospital->id)->pluck('id');
-                $checks = Check::whereIn('nurse_id', $nurses)->orderByDesc('id')->with(['nurse.userPerson.person', 'baby_incubator.baby.person'])->get();
+                $checksQuery->whereIn('nurse_id', $nurses);
             } else {
-                $checks = Check::where('nurse_id', $nurse->id)->orderByDesc('id')->with(['nurse.userPerson.person', 'baby_incubator.baby.person'])->get();
+                $checksQuery->where('nurse_id', $nurse->id);
             }
-
+    
+            $checks = $checksQuery->orderByDesc('id')->paginate(9);
+    
         } else if ($user->role == 'super-admin' || $user->role == 'admin') {
-            $checks = Check::orderByDesc('id')->with(['nurse.userPerson.person', 'baby_incubator.baby.person']);
-
+            $checksQuery = Check::query()->with(['nurse.userPerson.person', 'baby_incubator.baby.person']);
+    
             if ($request->hospital_id) {
                 $nurses = Nurse::where('hospital_id', $request->hospital_id)->pluck('id');
-                $checks->whereIn('nurse_id', $nurses);
+                $checksQuery->whereIn('nurse_id', $nurses);
             }
-
+    
             if ($request->nurse_id) {
-                $checks->where('nurse_id', $request->nurse_id);
+                $checksQuery->where('nurse_id', $request->nurse_id);
             }
-
+    
             if ($request->baby_id) {
-                $checks->whereHas('baby_incubator.baby', function ($query) use ($request) {
+                $checksQuery->whereHas('baby_incubator.baby', function ($query) use ($request) {
                     $query->where('id', $request->baby_id);
                 });
             }
-
+    
             if ($request->incubator_id) {
-                $checks->where('baby_incubator_id', $request->incubator_id);
+                $checksQuery->where('baby_incubator_id', $request->incubator_id);
             }
-
+    
             if ($request->date1 && $request->date2) {
-                $checks->whereBetween('created_at', [$request->date1, $request->date2]);
+                $checksQuery->whereBetween('created_at', [$request->date1, $request->date2]);
             }
-
-            $checks = $checks->paginate(9);
+    
+            $checks = $checksQuery->orderByDesc('id')->paginate(6);
+        } else {
+            return response()->json([
+                'message' => 'Unauthorized'
+            ], 403);
         }
-
+    
         if ($checks->isEmpty()) {
             return response()->json([
                 'message' => 'No checks found'
             ], 404);
         }
-
-        $data = $checks->map(function ($check) use ($user) {
+    
+        $checks->getCollection()->transform(function ($check) use ($user) {
             $nurse = $check->nurse;
             $nursePerson = $nurse ? $nurse->userPerson->person : null;
-
+    
             $nurseFullName = null;
             if ($user->role != 'nurse') {
                 $nurseFullName = $nursePerson ? $nursePerson->name . ' ' . $nursePerson->last_name_1 . ' ' . $nursePerson->last_name_2 : null;
             }
-
+    
             $babyIncubator = $check->baby_incubator;
             $baby = $babyIncubator ? $babyIncubator->baby : null;
             $babyPerson = $baby ? $baby->person : null;
             $babyFullName = $babyPerson ? $babyPerson->name . ' ' . $babyPerson->last_name_1 . ' ' . $babyPerson->last_name_2 : null;
             $incubator = $babyIncubator ? $babyIncubator->incubator->id : null;
+            
             return [
                 'check_id' => $check->id,
                 'title' => $check->title,
@@ -112,18 +124,12 @@ class ChecksController extends Controller
                 'nurse' => $nurseFullName,
                 'baby' => $babyFullName,
                 'incubator' => $incubator,
-                'created_at' => $check->created_at
+                'created_at' => $check->created_at->format('Y-m-d H:i:s'),
             ];
         });
-
-        if (!$checks) {
-            return response()->json([
-                'msg' => 'No Data Found'
-            ], 404);
-        }
-
+    
         return response()->json([
-            'data' => $data
+            'data' => $checks
         ], 200);
     }
     public function store(Request $request)
